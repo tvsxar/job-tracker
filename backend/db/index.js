744480e439b4c4d7b6f7ac5eb9ca896d490194d5
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-03-27 10:21:13 +0200

    feat: setup MySQL connection (Railway)

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,15 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  uri: process.env.MYSQL_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
 const testConnection = async () => {
