eb6dff392e73f3f18c079f155bebe2a6bfa8c3e9
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-03-29 20:25:36 +0300

    feat: create HomePage basic component & add routing

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className='px-4 sm:px-12 lg:px-25 xl:px-25'>
+        <div className="flex flex-col gap-16 sm:gap-16 py-8 sm:py-12">
+            HomePage
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
