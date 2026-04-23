d51b2fc22e664ffd53732db8bb7ab5ab80505933
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-04-23 17:01:36 +0300

    feat: create JobList component & add to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
+import JobsList from "../components/JobsList";
+
 function HomePage() {
   return (
-    <div className='px-4 sm:px-12 lg:px-25 xl:px-25'>
-        <div className="flex flex-col gap-16 sm:gap-16 py-8 sm:py-12">
-            HomePage
-        </div>
+    <div className='px-4 sm:px-12 lg:px-25 xl:px-25 bg-gray-100 flex flex-col gap-16 sm:gap-16 py-8 sm:py-12 min-h-[85vh]'>
+      <JobsList />
     </div>
   )
 }
