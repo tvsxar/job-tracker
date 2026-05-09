84ce25593f2ac0b84704cbccbf5644c92cb85283
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-05-09 23:24:43 +0300

    feat: add status change functions

--- frontend/src/components/JobCard.tsx
+++ frontend/src/components/JobCard.tsx
@@ -1,35 +1,65 @@
-import type { Job } from '../types';
+import { useState } from 'react';
+import type { Job, JobStatus } from '../types';
 
 interface JobCardProps {
-    job: Job
+    job: Job;
+    toggleMenu: () => void;
+    setIsOpen: (arg: boolean) => void;
+    isOpen: boolean;
 }
 
-function JobCard({ job }: JobCardProps) {
+function JobCard({ job, toggleMenu, setIsOpen, isOpen }: JobCardProps) {
+    const [status, setStatus] = useState(job.status);
+
     const bgColors = {
-        'Applied': 'bg-blue-200',
-        'Interview': 'bg-orange-200',
-        'Offer': 'bg-green-200',
-        'Rejected': 'bg-red-200'
+        'applied': 'bg-blue-200',
+        'interview': 'bg-orange-200',
+        'offer': 'bg-green-200',
+        'rejected': 'bg-red-200'
     }
 
     const textColors = {
-        'Applied': 'text-blue-500',
-        'Interview': 'text-orange-500',
-        'Offer': 'text-green-500',
-        'Rejected': 'text-red-500'
+        'applied': 'text-blue-500',
+        'interview': 'text-orange-500',
+        'offer': 'text-green-500',
+        'rejected': 'text-red-500'
     }
+
+    const handleStatusChange = (newStatus: JobStatus) => {
+        setStatus(newStatus);
+        setIsOpen(false);
+    };
+
+    const formatStatus = (s: string) =>
+        s.charAt(0).toUpperCase() + s.slice(1);
 
     return (
         <div className="border rounded-lg p-4 flex flex-col gap-2 border-gray-300 bg-white">
-            <div className="flex items-center justify-between mb-3">
+            <div className="flex items-center justify-between mb-3 relative">
                 <h2 className="font-semibold text-lg">{job.title}</h2>
 
-                <div className={`${bgColors[job.status]} ${textColors[job.status]}  py-0.5 px-1 rounded-lg`}>
-                    <p>{job.status}</p>
+                <div 
+                onClick={toggleMenu}
+                className={`${bgColors[status]} ${textColors[status]}  py-0.5 px-1 rounded-lg cursor-pointer hover:scale-102`}>
+                    {formatStatus(status)}
                 </div>
+
+                {isOpen && (
+                    <div className="origin-top-right absolute right-0 mt-2 w-24 
+                    rounded-lg shadow-lg bg-white ring-1 ring-gray-300 ring-opacity-5
+                    focus:outline-none"
+                        role="menu">
+                        <div role="none">
+                            <p onClick={() => handleStatusChange('applied')} className='px-1 hover:bg-gray-200 cursor-pointer rounded-t-lg border-b-gray-200'>Applied</p>
+                            <p onClick={() => handleStatusChange('interview')} className='px-1 hover:bg-gray-200 cursor-pointer'>Interview</p>
+                            <p onClick={() => handleStatusChange('offer')} className='px-1 hover:bg-gray-200 cursor-pointer'>Offer</p>
+                            <p onClick={() => handleStatusChange('rejected')} className='px-1 hover:bg-gray-200 cursor-pointer rounded-b-lg'>Rejected</p>
+                        </div>
+                    </div>
+                )}
             </div>
 
-            <p className='text-gray-600'>{job.company}</p>
+            <p className='text-gray-600 text-sm'>{job.company}</p>
 
             <p className="font-medium text-lg">${job.salary}</p>
         </div>
--- frontend/src/components/JobsList.tsx
+++ frontend/src/components/JobsList.tsx
@@ -1,49 +1,54 @@
 import JobCard from "./JobCard";
 import type { Job } from '../types';
+import { useState } from "react";
 
 function JobsList() {
+    const [isOpen, setIsOpen] = useState(false);
+
     const jobs: Job[] = [
         {
             id: 1,
             title: 'Software Engineer',
             company: 'Google',
             salary: 120000,
-            status: 'Applied'
+            status: 'applied'
         },
         {
             id: 2,
             title: 'Accountant',
             company: 'Amazon',
             salary: 110000,
-            status: 'Interview'
+            status: 'interview'
         },
         {
             id: 3,
             title: 'Software Engineer',
             company: 'Apple',
             salary: 90000,
-            status: 'Rejected'
+            status: 'rejected'
         },
         {
             id: 4,
             title: 'Data Scientist',
             company: 'Google',
             salary: 100000,
-            status: 'Offer'
+            status: 'offer'
         },
         {
             id: 5,
             title: 'Designer',
             company: 'Apple',
             salary: 80000,
-            status: 'Applied'
+            status: 'applied'
         },
     ];
+
+    const toggleMenu = () => setIsOpen(prev => !prev);;
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {jobs.map(job => (
-                <JobCard key={job.id} job={job} />
+                <JobCard key={job.id} job={job} isOpen={isOpen} toggleMenu={toggleMenu} setIsOpen={setIsOpen} />
             ))}
         </div>
     )
